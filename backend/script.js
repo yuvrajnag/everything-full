7aa512c258d5bdde1560dfe738ca0f8f9cbded3a
yuvrajnag <yuvrajnag.kodithyala@gmail.com>
2026-06-28 22:45:16 +0530

    update banners to e and x

---
+++ backend/script.js
@@ -0,0 +1,1 @@
+const { PrismaClient } = require('@prisma/client'); const prisma = new PrismaClient(); async function main() { await prisma.user.updateMany({ data: { role: 'SUPER_ADMIN' } }); console.log('All users upgraded to SUPER_ADMIN'); } main().catch(console.error).finally(()=>prisma.$disconnect());
